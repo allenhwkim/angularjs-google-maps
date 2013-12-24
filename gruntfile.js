4a796c21ef6b6e6cd75ef847c9503f4c7491294a
Allen Kim <allen.kim@kineticsocial.com>
2013-12-23 23:07:45 -0500

    added grunt-bump

--- gruntfile.js
+++ gruntfile.js
@@ -20,7 +20,24 @@ module.exports = function (grunt) {
         flatten: true,   // remove all unnecessary nesting
         ext: '.min.js'   // replace .js to .min.js
       }
+    },
+    
+    bump: {
+      options: {
+        files: ['package.json'],
+        updateConfigs: [],
+        commit: true,
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: ['package.json'], // '-a' for all files
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: true,
+        pushTo: '',
+        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d' // options to use with '$ git describe'
+      }
     }
+
   });
 
   // load plugins
@@ -28,6 +45,7 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-bump');
 
   grunt.registerTask('min', ['clean', 'concat', 'uglify']);
 };
