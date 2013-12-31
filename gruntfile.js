79a07d71e82bb6e054db5241b88333e841668c04
Allen Kim <allen.kim@kineticsocial.com>
2013-12-31 11:28:31 -0500

    updated gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -54,17 +54,7 @@ module.exports = function (grunt) {
 
    bump: {
      options: {
-       //files: ['package.json'],
-       //updateConfigs: [],
-       //commit: true,
-       //commitMessage: 'Release v%VERSION%',
-       //commitFiles: ['package.json'], // '-a' for all files
-       //createTag: true,
-       //tagName: 'v%VERSION%',
-       //tagMessage: 'Version %VERSION%',
-       //push: true,
-       pushTo: '',
-       //gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d' // options to use with '$ git describe'
+       pushTo: ''
      }
    }, 
 
