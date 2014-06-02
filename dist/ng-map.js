1cbb03ac083ec6a397db91477d658ba7a380adbb
Allen Kim <allenhwkim@gmail.com>
2014-06-02 17:18:31 -0400

    issue #2, simplified attribute values

--- dist/ng-map.js
+++ dist/ng-map.js
@@ -421,19 +421,28 @@ ngMap.provider('Attr2Options', function() {
                   } catch(e) {
                     options[key] = val;
                   } 
+                // 4. Object Expression. i.e. MayTypeId.HYBRID 
                 } else if (val.match(/^[A-Z][a-zA-Z0-9]+\.[A-Z]+$/)) {
                   try {
                     options[key] = scope.$eval("google.maps."+val);
                   } catch(e) {
                     options[key] = val;
                   } 
+                // 5. Object Expression. i.e. HYBRID 
+                } else if (val.match(/^[A-Z]+$/)) {
+                  try {
+                    var capitializedKey = key.charAt(0).toUpperCase() + key.slice(1);
+                    options[key] = scope.$eval("google.maps."+capitializedKey+"."+val);
+                  } catch(e) {
+                    options[key] = val;
+                  } 
                 } else {
                   options[key] = val;
                 }
-              }
-            }
-          }
-        }
+              } // catch(err2)
+            } // catch(err)
+          } // if (attrs[key])
+        } //for(var key in attrs)
         return options;
       };
 
