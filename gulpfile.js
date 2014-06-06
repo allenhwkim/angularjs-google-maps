c9f94451c5449b67bfbeef82a645af0dc2082b74
Allen Kim <allenhwkim@gmail.com>
2014-06-06 17:44:54 -0400

    Version 0.6.2

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,30 @@ var runSequence = require('run-sequence');
 var gutil = require('gulp-util');
 var through = require('through2');
 var replace = require('gulp-replace');
+var tap = require('gulp-tap');
+var shell = require('gulp-shell');
+var bump = require('gulp-bump');
+var bumpVersion = function(type) {
+  type = type || 'patch';
+  var version = '';
+  gulp.src(['./bower.json', './package.json'])
+    .pipe(bump({type: type}))
+    .pipe(gulp.dest('./'))
+    .pipe(tap(function(file, t) {
+      version = JSON.parse(file.contents.toString()).version;
+    })).on('end', function() {
+      var color = gutil.colors;
+      gulp.src('')
+        .pipe(shell([
+          'git commit --all --message "Version ' + version + '"',
+          (type != 'patch' ? 'git tag --annotate "v' + version + '" --message "Version ' + version + '"' : 'true')
+        ], {ignoreErrors: true}))
+        .pipe(tap(function() {
+          gutil.log(color.green("Version bumped to ") + color.yellow(version) + color.green(", don't forget to push!"));
+        }));
+    });
+
+};
 
 gulp.task('clean', function() {
   return gulp.src('bulid')
@@ -47,6 +71,11 @@ gulp.task('build-html', function() {
     ))
     .pipe(gulp.dest('./build'));
 });
+
+gulp.task('bump', function() { bumpVersion('patch'); });
+gulp.task('bump:patch', function() { bumpVersion('patch'); });
+gulp.task('bump:minor', function() { bumpVersion('minor'); });
+gulp.task('bump:major', function() { bumpVersion('major'); });
 
 gulp.task('build', function(callback) {
   runSequence('clean', 'build-js', 'copy', 'build-html', callback);
