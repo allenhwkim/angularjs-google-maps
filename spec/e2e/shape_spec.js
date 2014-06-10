59d3689c9f5dda3ef545346a331884075c90db4d
Allen Kim <allenhwkim@gmail.com>
2014-06-10 09:13:35 -0400

    added wait for test

--- spec/e2e/shape_spec.js
+++ spec/e2e/shape_spec.js
@@ -3,6 +3,15 @@ describe('Shape', function() {
 
   it('sets shapes', function() {
     browser.get('shape.html');
+    browser.wait( function() {
+      return browser.executeScript( function() {
+        var el = document.querySelector("map");  
+        var scope = angular.element(el).scope();
+        return scope.map.getCenter().lat();
+      }).then(function(result) {
+        return result;
+      });
+    }, 5000);
     element(by.css("map")).evaluate('shapes[1].map.getCenter().lat()').then(function(lat) {
       expect(lat).toBeGreaterThan(0);
     });
