d73735456fb5172f254bdb20618848f9fb822746
Allen Kim <allenhwkim@gmail.com>
2014-06-11 16:42:04 -0400

    added googlemap-based application using world-wide starbucks

---
+++ scripts/get_starbucks.js
@@ -0,0 +1,16 @@
+var request = require("request")
+
+var base = "http://opendata.socrata.com/resource/92ua-293q.json";
+var stores = [];
+
+for (var i=0; i<21000; i+=1000) {
+  var url = base + "?$offset=" + i;
+  request({ url: url, json: true }, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      stores = stores.concat(body);
+    }
+    if (stores.length > 20000) {
+      console.log(JSON.stringify(stores));
+    }
+  })
+}
