84a1068ff78d5d99af0197c22acadc1a5a07c684
Allen Kim <allenhwkim@gmail.com>
2015-04-04 23:11:24 -0400

    Version 1.6.0

--- build/scripts/ng-map.debug.js
+++ build/scripts/ng-map.debug.js
@@ -1497,7 +1497,7 @@ ngMap.directive('mapType', ['Attr2Options', '$window', function(Attr2Options, $w
         var len = Object.keys(this.map[groupName]).length;
         this.map[groupName][obj.id || len] = obj;
         if (groupName != "infoWindows" && obj.setMap) { //infoWindow.setMap works like infoWindow.open
-          obj.setMap(this.map);
+          obj.setMap && obj.setMap(this.map);
         }
         if (obj.centered && obj.position) {
           this.map.setCenter(obj.position);
@@ -1506,7 +1506,7 @@ ngMap.directive('mapType', ['Attr2Options', '$window', function(Attr2Options, $w
         obj.groupName = groupName;
         this._objects.push(obj);
       }
-    }
+    };
 
     /**
      * Delete an object from the collection and remove from map
@@ -1523,7 +1523,7 @@ ngMap.directive('mapType', ['Attr2Options', '$window', function(Attr2Options, $w
       }
 
       /* delete from map */
-      obj.map && obj.setMap(null);          
+      obj.map && obj.setMap && obj.setMap(null);
     };
 
     /**
@@ -1999,6 +1999,98 @@ ngMap.directive('overlayMapType', ['Attr2Options', '$window', function(Attr2Opti
 
   angular.module('ngMap').directive('shape', shape);
 
+})();
+
+/**
+ * @ngdoc directive
+ * @name streetview-panorama
+ * @requires Attr2Options
+ * @description
+ *   Requires:  map directive
+ *   Restrict To:  Element
+ *
+ * @param container Optional, id or css selector, if given, streetview will be in the given html element
+ * @param {String} &lt;StreetViewPanoramaOption> Any Google StreetViewPanorama options, 
+ *        https://developers.google.com/maps/documentation/javascript/reference?csw=1#StreetViewPanoramaOptions
+ * @param {String} &lt;StreetViewPanoramaEvent> Any Google StreetViewPanorama events, 
+ *        https://developers.google.com/maps/documentation/javascript/reference#StreetViewPanorama 
+ *
+ * @example
+ *   <map zoom="11" center="[40.688738,-74.043871]" >
+ *     <street-view-panorama
+ *       click-to-go="true"
+ *       disable-default-ui="true"
+ *       disable-double-click-zoom="true"
+ *       enable-close-button="true"
+ *       pano="my-pano"
+ *       position="40.688738,-74.043871"
+ *       pov="{heading:0, pitch: 90}"
+ *       scrollwheel="false"
+ *       visible="true">
+ *     </street-view-panorama>
+ *   </map>
+ */
+/* global google */
+(function() {
+  'use strict';
+   
+  var streetViewPanorama = function(Attr2Options) {
+    var parser = Attr2Options;
+  
+    var getStreetViewPanorama = function(map, options, events) {
+      var svp, container;
+      if (options.container) {
+        container = document.getElementById(options.container);
+        container = container || document.querySelector(options.container);
+      }
+      if (container) {
+        svp = new google.maps.StreetViewPanorama(container, options);
+      } else {
+        svp = map.getStreetView();
+        svp.setOptions(options);
+      }
+
+      for (var eventName in events) {
+        eventName &&
+          google.maps.event.addListener(svp, eventName, events[eventName]);
+      }
+      return svp;
+    };
+
+    var linkFunc = function(scope, element, attrs, mapController) {
+      var orgAttrs = parser.orgAttributes(element);
+      var filtered = parser.filter(attrs);
+      var options = parser.getOptions(filtered);
+      var controlOptions = parser.getControlOptions(filtered);
+      var svpOptions = angular.extend(options, controlOptions);
+
+      var svpEvents = parser.getEvents(scope, filtered);
+      console.log('street-view-panorama', 
+        'options', svpOptions, 'events', svpEvents);
+ 
+      scope.$on('mapInitialized', function(evt, map) {
+        var svp = getStreetViewPanorama(map, svpOptions, svpEvents);
+        console.log('svp', svp);
+
+        map.setStreetView(svp);
+        (!svp.getPosition()) && svp.setPosition(map.getCenter());
+        google.maps.event.addListener(svp, 'position_changed', function() {
+          map.setCenter(svp.getPosition());
+        });
+      });
+
+    }; //link
+
+    return {
+      restrict: 'E',
+      require: '^map',
+      link: linkFunc
+    };
+
+  };
+  streetViewPanorama.$inject = ['Attr2Options'];
+
+  angular.module('ngMap').directive('streetViewPanorama', streetViewPanorama);
 })();
 
 /**
