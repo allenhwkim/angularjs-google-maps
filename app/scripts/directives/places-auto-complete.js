728cd8d6cfe0e27fa57ee74ac562fbf2b71f4348
Allen Kim <allenhwkim@gmail.com>
2015-04-08 23:13:08 -0400

    issue #174, made places-auto-complete more useful

--- app/scripts/directives/places-auto-complete.js
+++ app/scripts/directives/places-auto-complete.js
@@ -19,26 +19,42 @@
 (function() {
   'use strict';
 
-  var placesAutoComplete = function(Attr2Options) {
+  var placesAutoComplete = function(Attr2Options, $timeout) {
     var parser = Attr2Options;
 
-    var linkFunc = function(scope, element, attrs) {
+    var linkFunc = function(scope, element, attrs, ngModelCtrl) {
       var filtered = parser.filter(attrs);
       var options = parser.getOptions(filtered);
       var events = parser.getEvents(scope, filtered);
+      console.log('autocomplete options', options, 'events', events);
       var autocomplete = new google.maps.places.Autocomplete(element[0], options);
       for (var eventName in events) {
         google.maps.event.addListener(autocomplete, eventName, events[eventName]);
       }
+      element[0].addEventListener('change', function() {
+        $timeout(function(){
+          ngModelCtrl && ngModelCtrl.$setViewValue(element.val());
+        },100);
+      });
+
+      attrs.$observe('types', function(val) {
+        if (val) {
+          console.log('observing types', val);
+          var optionValue = parser.toOptionValue(val, {key: 'types'});
+          console.log('setting types with value', optionValue);
+          autocomplete.setTypes(optionValue);
+        }
+      });
     };
 
     return {
       restrict: 'A',
+      require: '?ngModel',
       link: linkFunc
     };
   };
 
-  placesAutoComplete.$inject = ['Attr2Options'];
+  placesAutoComplete.$inject = ['Attr2Options', '$timeout'];
   angular.module('ngMap').directive('placesAutoComplete', placesAutoComplete); 
 
 })();
