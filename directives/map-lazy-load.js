d5ab042cbe6fbb5145a5174c2bc7e16d5305c30d
Allen Kim <allenhwkim@gmail.com>
2015-07-20 20:34:40 -0400

    refactored code

--- directives/map-lazy-load.js
+++ directives/map-lazy-load.js
@@ -22,42 +22,52 @@
  */
 (function() {
   'use strict';
+  var $timeout, $compile, src, savedHtml;
 
-  angular.module('ngMap').directive('mapLazyLoad', ['$compile', '$timeout', function($compile, $timeout) {
-    'use strict';
-    var directiveDefinitionObject = {
-      compile: function(tElement, tAttrs) {
-        (!tAttrs.mapLazyLoad) && console.error('requires src with map-lazy-load');
-        var savedHtml = tElement.html(), src = tAttrs.mapLazyLoad;
-        /**
-         * if already loaded, stop processing it
-         */
-        if (document.querySelector('script[src="'+src+'?callback=lazyLoadCallback"]')) {
-          return false;
-        }
-
-        tElement.html('');  // will compile again after script is loaded
-        return {
-          pre: function(scope, element, attrs) {
-            window.lazyLoadCallback = function() {
-              console.log('script loaded,' + src);
-              $timeout(function() { /* give some time to load */
-                element.html(savedHtml);
-                $compile(element.contents())(scope);
-              }, 100);
-            };
-            if(window.google === undefined || window.google.maps === undefined) {
-              var scriptEl = document.createElement('script');
-              scriptEl.src = src + (src.indexOf('?') > -1 ? '&' : '?') + 'callback=lazyLoadCallback';
-              document.body.appendChild(scriptEl);
-            } else {
-              element.html(savedHtml);
-              $compile(element.contents())(scope);
-            }
-          }
-        };
-      }
+  var preLinkFunc = function(scope, element, attrs) {
+    window.lazyLoadCallback = function() {
+      console.log('script loaded,' + src);
+      $timeout(function() { /* give some time to load */
+        element.html(savedHtml);
+        $compile(element.contents())(scope);
+      }, 100);
     };
-    return directiveDefinitionObject;
-  }]);
+
+    if(window.google === undefined || window.google.maps === undefined) {
+      var scriptEl = document.createElement('script');
+      scriptEl.src = src + (src.indexOf('?') > -1 ? '&' : '?') + 'callback=lazyLoadCallback';
+      document.body.appendChild(scriptEl);
+    } else {
+      element.html(savedHtml);
+      $compile(element.contents())(scope);
+    }
+  };
+
+  var compileFunc = function(tElement, tAttrs) {
+
+    (!tAttrs.mapLazyLoad) && console.error('requires src with map-lazy-load');
+    savedHtml = tElement.html(); 
+    src = tAttrs.mapLazyLoad;
+
+    /**
+     * if already loaded, stop processing it
+     */
+    if (document.querySelector('script[src="'+src+'?callback=lazyLoadCallback"]')) {
+      return false;
+    }
+
+    tElement.html('');  // will compile again after script is loaded
+    return {
+      pre: preLinkFunc
+    };
+  };
+
+  var mapLazyLoad = function(_$compile_, _$timeout_) {
+    $compile = _$compile_, $timeout = _$timeout_;
+    return {
+      compile: compileFunc
+    }
+  };
+
+  angular.module('ngMap').directive('mapLazyLoad', mapLazyLoad);
 })();
