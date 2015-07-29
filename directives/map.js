552db0063e9d0c0f970071b7422395e983b8bdb4
Xavier John <xavierjohn@hotmail.com>
2015-07-29 11:44:16 -0700

    Bug Fix Auto zoom. Updated Doc

--- directives/map.js
+++ directives/map.js
@@ -15,26 +15,26 @@
  *     Element
  *
  * @param {Expression} geo-callback if center is an address or current location, the expression is will be executed when geo-lookup is successful. e.g., geo-callback="showMyStoreInfo()"
- * @param {Array} geo-fallback-center 
+ * @param {Array} geo-fallback-center
  *    The center of map incase geolocation failed. i.e. [0,0]
  * @param {Boolean} zoom-to-include-markers
  *    When true, map boundary will be changed automatially to include all markers when initialized
  * @param {Boolean} default-style
  *    When false, the default styling, `display:block;height:300px`, will be ignored.
- * @param {String} init-event The name of event to initialize this map. 
+ * @param {String} init-event The name of event to initialize this map.
  *    If this option is given, the map won't be initialized until the event is received.
- *    To invoke the event, use $scope.$emit or $scope.$broacast. 
+ *    To invoke the event, use $scope.$emit or $scope.$broacast.
  *    i.e. <map init-event="init-map" ng-click="$emit('init-map')" center=... ></map>
- * @param {String} &lt;MapOption> Any Google map options, 
+ * @param {String} &lt;MapOption> Any Google map options,
  *    https://developers.google.com/maps/documentation/javascript/reference?csw=1#MapOptions
- * @param {String} &lt;MapEvent> Any Google map events, 
+ * @param {String} &lt;MapEvent> Any Google map events,
  *    https://rawgit.com/allenhwkim/angularjs-google-maps/master/build/map_events.html
  * @example
  * Usage:
  *   <map MAP_OPTIONS_OR_MAP_EVENTS ..>
  *     ... Any children directives
  *   </map>
- * 
+ *
  * Example:
  *   <map center="[40.74, -74.18]" on-click="doThat()">
  *   </map>
@@ -43,20 +43,20 @@
  *   </map>
  */
 /* global google */
-(function() {
+(function () {
   'use strict';
 
-  function getStyle(el,styleProp) {
+  function getStyle(el, styleProp) {
     var y;
     if (el.currentStyle) {
       y = el.currentStyle[styleProp];
     } else if (window.getComputedStyle) {
-      y = document.defaultView.getComputedStyle(el,null).getPropertyValue(styleProp);
+      y = document.defaultView.getComputedStyle(el, null).getPropertyValue(styleProp);
     }
     return y;
   }
 
-  var mapDirective = function(Attr2Options, $timeout, $parse) {
+  var mapDirective = function (Attr2Options, $timeout, $parse) {
     var parser = Attr2Options;
 
     /**
@@ -67,7 +67,7 @@
      * @param {Hash} attrs
      * @ctrl {MapController} ctrl
      */
-    var linkFunc = function(scope, element, attrs, ctrl) {
+    var linkFunc = function (scope, element, attrs, ctrl) {
       var orgAttrs = parser.orgAttributes(element);
 
       scope.google = google;  //used by $scope.eval in Attr2Options to avoid eval()
@@ -86,17 +86,17 @@
        */
       if (attrs.defaultStyle !== 'false') {
         if (getStyle(element[0], 'display') != "block") {
-          element.css('display','block');
+          element.css('display', 'block');
         }
         if (getStyle(element[0], 'height').match(/^(0|auto)/)) {
-          element.css('height','300px');
+          element.css('height', '300px');
         }
       }
 
       /**
        * disable drag event
        */
-      element[0].addEventListener('dragstart', function(event) {
+      element[0].addEventListener('dragstart', function (event) {
         event.preventDefault();
         return false;
       });
@@ -104,15 +104,15 @@
       /**
        * initialize function
        */
-      var initializeMap = function(mapOptions, mapEvents) {
+      var initializeMap = function (mapOptions, mapEvents) {
         var map = new google.maps.Map(el, {});
         map.markers = {};
         map.shapes = {};
-       
+
         /**
          * resize the map to prevent showing partially, in case intialized too early
          */
-        $timeout(function() {
+        $timeout(function () {
           google.maps.event.trigger(map, "resize");
         });
 
@@ -122,14 +122,14 @@
         mapOptions.zoom = mapOptions.zoom || 15;
         var center = mapOptions.center;
         if (!center) {
-          mapOptions.center = new google.maps.LatLng(0,0);
+          mapOptions.center = new google.maps.LatLng(0, 0);
         } else if (!(center instanceof google.maps.LatLng)) {
           delete mapOptions.center;
-          ctrl.getGeoLocation(center).then(function(latlng) {
+          ctrl.getGeoLocation(center).then(function (latlng) {
             map.setCenter(latlng);
             var geoCallback = attrs.geoCallback;
             geoCallback && $parse(geoCallback)(scope);
-          }, function(error) {
+          }, function (error) {
             map.setCenter(options.geoFallbackCenter);
           });
         }
@@ -151,10 +151,11 @@
 
         /**
          * set controller and set objects
-         * so that map can be used by other directives; marker or shape 
+         * so that map can be used by other directives; marker or shape
          * ctrl._objects are gathered when marker and shape are initialized before map is set
          */
-        ctrl.map = map;   /* so that map can be used by other directives; marker or shape */
+        ctrl.map = map;
+        /* so that map can be used by other directives; marker or shape */
         ctrl.addObjects(ctrl._objects);
 
         // /* providing method to add a marker used by user scope */
@@ -168,16 +169,15 @@
          */
         scope.map = map;
         scope.map.scope = scope;
-        google.maps.event.addListenerOnce(map, "idle", function() {
-          scope.$emit('mapInitialized', map);  
-          if (attrs.zoomToIncludeMarkers == 'auto') {
-            scope.$on('objectChanged', function(evt, msg) {
-              console.log('objectChanged name', msg);
-              msg[0] == 'markers' && ctrl.zoomToIncludeMarkers();
-            });
-          }else if (attrs.zoomToIncludeMarkers) {
-            console.log('zoomToIncludeMarkers');
+        google.maps.event.addListenerOnce(map, "idle", function () {
+          scope.$emit('mapInitialized', map);
+          if (attrs.zoomToIncludeMarkers) {
             ctrl.zoomToIncludeMarkers();
+            if (attrs.zoomToIncludeMarkers == 'auto') {
+              scope.$on('objectChanged', function (evt, msg) {
+                msg[0] == 'markers' && ctrl.zoomToIncludeMarkers();
+              });
+            }
           }
         });
       }; // function initializeMap()
@@ -193,7 +193,7 @@
       console.log("filtered", filtered, "mapOptions", mapOptions, 'mapEvents', mapEvents);
 
       if (attrs.initEvent) { // allows controlled initialization
-        scope.$on(attrs.initEvent, function() {
+        scope.$on(attrs.initEvent, function () {
           !ctrl.map && initializeMap(mapOptions, mapEvents); // init if not done
         });
       } else {
@@ -205,7 +205,7 @@
       restrict: 'AE',
       controller: 'MapController',
       link: linkFunc
-    }; 
+    };
   };
 
   angular.module('ngMap').directive('map', ['Attr2Options', '$timeout', '$parse', mapDirective]);
