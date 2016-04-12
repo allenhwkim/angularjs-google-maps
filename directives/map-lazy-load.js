3cdfde0c1a4f937e397d8a6bed680419092cb736
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2016-04-12 19:34:10 +0100

    [map-lazy-load] avoid using the same content of savedHtml.

--- directives/map-lazy-load.js
+++ directives/map-lazy-load.js
@@ -36,7 +36,7 @@
 /* global window, document */
 (function() {
   'use strict';
-  var $timeout, $compile, src, savedHtml, elements = [];
+  var $timeout, $compile, src, savedHtml = [], elements = [];
 
   var preLinkFunc = function(scope, element, attrs) {
     var mapsUrl = attrs.mapLazyLoadParams || attrs.mapLazyLoad;
@@ -45,7 +45,7 @@
       elements.push({
         scope: scope,
         element: element,
-        savedHtml: savedHtml,
+        savedHtml: savedHtml[elements.length],
       });
 
       window.lazyLoadCallback = function() {
@@ -77,7 +77,7 @@
   var compileFunc = function(tElement, tAttrs) {
 
     (!tAttrs.mapLazyLoad) && console.error('requires src with map-lazy-load');
-    savedHtml = tElement.html();
+    savedHtml.push(tElement.html());
     src = tAttrs.mapLazyLoad;
 
     /**
