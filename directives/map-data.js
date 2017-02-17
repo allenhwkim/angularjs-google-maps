4e744f07b78fb1971f0011a078426aad5343988f
gbx <guillaume.broux@code101.fr>
2017-02-17 09:58:02 +0100

    Pass map.id to getMap()

--- directives/map-data.js
+++ directives/map-data.js
@@ -26,13 +26,14 @@
       restrict: 'E',
       require: ['?^map','?^ngMap'],
 
-      link: function(scope, element, attrs) {
+      link: function(scope, element, attrs, mapController) {
+        mapController = mapController[0] || mapController[1];
         var filtered = parser.filter(attrs);
         var options = parser.getOptions(filtered, {scope: scope});
         var events = parser.getEvents(scope, filtered, events);
 
         console.log('map-data options', options);
-        NgMap.getMap().then(function(map) {
+        NgMap.getMap(mapController.map.id).then(function(map) {
           //options
           for (var key in options) {
             var val = options[key];
